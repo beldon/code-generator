ab2fc454729a21ae7793b14677d7a2a5c642301f
beldon <1047325205@qq.com>
2017-08-24 18:34:27 +0800

    fix bug

--- src/test/java/me/beldon/GenerateApplicationTests.java
+++ src/test/java/me/beldon/GenerateApplicationTests.java
@@ -1,5 +1,6 @@
 package me.beldon;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,6 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GenerateApplicationTests {
+    @Test
+    public void contextLoads() {
 
+    }
 
 }
